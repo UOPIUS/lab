037f87a4e9eef3ca54e57ba53e64875c96c5411b
Otseogbu Pius <unogwupius@yahoo.com>
2024-12-11 09:49:17 +0100

    allow doctors and cashier access the inventory menu

--- menu.php
+++ menu.php
@@ -217,14 +217,15 @@ endswitch;
                 <?php endif; ?>
                 <?php if ($_SESSION['role_id'] == 107 || $_SESSION['role_id'] == 106 || $_SESSION['role_id'] == 103): ?>
                     <div class="sb-sidenav-menu-heading">Inventory</div>
-                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts"
-                        aria-expanded="false" aria-controls="collapseLayouts">
+                    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#inventoryLayoutCollapse"
+                        aria-expanded="false" aria-controls="inventoryLayoutCollapse">
                         <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
                         Manage inventory
                         <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
 
                     </a>
-                    <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+                    <div class="collapse" id="inventoryLayoutCollapse" aria-labelledby="headingInventory"
+                        data-parent="#sidenavAccordion">
                         <nav class="sb-sidenav-menu-nested nav">
                             <a class="nav-link" href="/inventory_category/categories.php">Category</a>
                             <a class="nav-link" href="/inventory_category/unit.php">Units</a>
